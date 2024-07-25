7d81d69ab93f139582ea3c6fac73c481a8be5ce9
snehavats1404 <snehavats987@gmail.com>
2024-07-25 12:28:15 +0530

    Update Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -17,7 +17,7 @@ const Info = () => {
 
         <div className="about_box">
         <i class='bx bx-support about_icon' ></i>
-            <h3 className="about_title">Support</h3>
+            <h3 className="about_title">Available</h3>
             <span className="about_subtitle">Online 24/7</span>
         </div>
 
