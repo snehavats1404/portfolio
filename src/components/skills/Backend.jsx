1f7bbd0752b138d0df0101722c64a0f31c69ff6c
snehavats1404 <snehavats987@gmail.com>
2025-01-11 18:36:15 +0530

    Update Backend.jsx

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,64 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 const Backend = () => {
   return (
     <div className="skills_content">
-        <h3 className="skills_title"></h3>
-        <div className="skills_box">
-            <div className="skills_group">
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">C++ & C </h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">Node Js</h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">Python</h3>
-                        <span className='skills_level'>Intermediate</span>
-                    </div>
-                </div>
-
-
-
-                
+      <h3 className="skills_title">Backend Skills</h3>
+      <div className="skills_box">
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">C++ & C</h3>
+              <span className='skills_level'>Advanced</span>
             </div>
-
-            <div className="skills_group">
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">Express</h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-              
-              <div className="skills_group">
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">MongoDb</h3>
-                        <span className='skills_level'>Intermediate</span>
-                    </div>
-                </div>
-
-                
-
+          </div>
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">Node.js</h3>
+              <span className='skills_level'>Advanced</span>
             </div>
+          </div>
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">Python</h3>
+              <span className='skills_level'>Intermediate</span>
+            </div>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Backend
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">Express</h3>
+              <span className='skills_level'>Advanced</span>
+            </div>
+          </div>
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">MongoDB</h3>
+              <span className='skills_level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
