f926a30f68c483ad194925d34234db84dc930ee4
snehavats1404 <snehavats987@gmail.com>
2025-01-11 18:36:58 +0530

    Update Frontend.jsx

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,73 +1,64 @@
-import React from 'react'
+import React from 'react';
 
 const Frontend = () => {
   return (
     <div className="skills_content">
-        <h3 className="skills_title">Technical skills</h3>
-        <div className="skills_box">
-            <div className="skills_group">
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">HTML5</h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">TailwindCSS</h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">JavaScript</h3>
-                        <span className='skills_level'>Basic</span>
-                    </div>
-                </div>
-
-
-
-                
+      <h3 className="skills_title">Technical Skills</h3>
+      <div className="skills_box">
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">HTML5</h3>
+              <span className='skills_level'>Advanced</span>
             </div>
+          </div>
 
-            <div className="skills_group">
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">MySQL</h3>
-                        <span className='skills_level'>Intermediate</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">Data Structures & ALgorithm</h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills_name">ReactJS</h3>
-                        <span className='skills_level'>Advanced</span>
-                    </div>
-                </div>
-
-
-                
-                
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">TailwindCSS</h3>
+              <span className='skills_level'>Advanced</span>
             </div>
+          </div>
+
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">JavaScript</h3>
+              <span className='skills_level'>Basic</span>
+            </div>
+          </div>
         </div>
-    </div>
-    
-  )
-}
 
-export default Frontend
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">MySQL</h3>
+              <span className='skills_level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">Data Structures & Algorithms</h3>
+              <span className='skills_level'>Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills_data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills_name">ReactJS</h3>
+              <span className='skills_level'>Advanced</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frontend;
